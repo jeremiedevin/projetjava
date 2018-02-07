cdb6366f73c6c0ced3dd1b6292d7176dc85870a3
Jérémie DEVIN <jeremie.devin@epsi.fr>
2018-02-07 09:45:54 +0100

    new test

--- Pion.java
+++ Pion.java
@@ -1,24 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Pion {
-  private int[] position;
+  private int x;
+  private int y;
   private int health;
   private char morale;
   private int melee;
   private int missile;
 
-  public int[] getPosition(){
-    return this.position;
+  public int getX(){
+    return this.x;
+  }
+
+  public int getY(){
+    return this.y;
   }
 
   public void setPosition(int x, int y){
-    int[] pos = {x,y};
-    this.position = pos;
+    this.x = x;
+    this.y = y;
   }
 
 
   public void displayPion(Pion Pion){
-    for (int t:Pion.getPosition()){
-      System.out.print(t);
-    }
+
+    ImageIcon pion_soldat= new ImageIcon(getClass().getResource("img/pion_soldat.png"));
+    Image imgsold = pion_soldat.getImage();
+
+
+    g2.drawImage(imgsold,2,2,100,100,null);
+    System.out.print(Pion.getX());
   }
 
 
